16ed385781011279323ec1f94688d309b9caf639
Maciej Kuzma <maciej.kuzma@cloudware.pl>
2016-05-06 13:52:28 +0200

    wielkie zmiany

--- js/functions.js
+++ js/functions.js
@@ -3,14 +3,15 @@ $(window).scroll(function(){
   var wScroll = $(this).scrollTop();
 
   $('.image1').css({
-    'transform' : 'translate(0px, '+ wScroll /8 +'%)'
+    'transform' : 'translate(0px, '+ wScroll /20 +'%)'
   });
 
   $('.image2').css({
     'transform' : 'translate(0px, '+ wScroll /6 +'%)'
   });
 
-  
+   $('.largest-window').css({
+    'transform' : 'translate(23%, '+ wScroll /70 +'%)'
+  });
 
-
-});
+   });
